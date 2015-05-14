a0908fbb0f89c68106c74dabe345d20510756de4
leroi <leroidejesa@gmail.com>
2015-05-13 23:05:05 -0700

    update recipe page (recipe.erb) so users can see recipe ingredient list, as well as a form for adding new ingredients; add and/or update app.rb's code, accordingly

--- app.rb
+++ app.rb
@@ -27,5 +27,20 @@ end
 
 get('/recipes/:id') do
   @recipe = Recipe.find(params.fetch("id").to_i())
+  @ingredients = @recipe.ingredients()
   erb(:recipe)
+end
+
+post('/ingredients') do
+  ingredient = params.fetch("ingredient")
+  recipe_id = params.fetch("recipe_id").to_i()
+  @recipe = Recipe.find(recipe_id)
+  @new_ingredient = Ingredient.new({:description => ingredient, :recipe_id => recipe_id})
+  if @new_ingredient.save()
+    redirect to("/recipes/#{recipe_id}")
+  else
+    @recipes = Recipe.all()
+    @categories = Category.all()
+    erb(:index)
+  end
 end
