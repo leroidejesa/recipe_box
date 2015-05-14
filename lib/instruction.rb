a11e868c750354cf98b5dcbb928df9b21cb03931
leroi <leroidejesa@gmail.com>
2015-05-13 18:59:55 -0700

    update lib files: assign associations

--- lib/instruction.rb
+++ lib/instruction.rb
@@ -1,3 +1,3 @@
 class Instruction < ActiveRecord::Base
-
+  belongs_to(:recipe)
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,20 @@ require("bundler/setup")
 Bundler.require(:default, :test)
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Category.all().each() do |category|
+      category.destroy()
+    end
+    Ingredient.all().each() do |ingredient|
+      ingredient.destroy()
+    end
+    Instruction.all().each() do |instruction|
+      instruction.destroy()
+    end
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+  end
+end
